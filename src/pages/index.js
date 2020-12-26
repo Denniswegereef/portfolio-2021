66e09ea2f89ed99037edba533949e063fb1de39c
Denniswegereef <denniswegereef@gmail.com>
2020-12-26 14:32:16 +0100

    init next js to setup vercel

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Portfolio 2021</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>New portfolio 2021</h1>
+      </main>
+
+      <footer />
+    </div>
+  )
+}
